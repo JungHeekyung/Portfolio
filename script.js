d86574d6abfa0827133047c0568f5a5b184a5b5f
JungHeekyung <kerryjhk@gmail.com>
2023-06-28 15:55:21 +0900

    메인 1차

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ $('#potion').on('click', () => {
   $('#gray').fadeOut({ display: 'none' });
 });
 
+$('#clock').on('click', () => {
+  $('#desk_01').css({ opacity: '0' }),
+    $('#desk_02').css({ opacity: '0' }),
+    $('#laptop_error').css({ display: 'none' }),
+    $('#stand_light').css({ display: 'none' }),
+    $('#stand_on').css({ display: 'none' }),
+    $('#sofa_cotton').css({ display: 'none' }),
+    $('#art').css({ transform: 'rotate(0deg)' + 'translateX(0px)' }),
+    $('#gray').fadeIn({ display: 'block' });
+});
 $('#art').on('mouseover', (e) => {
   $(e.target).css({ transform: 'rotate(5deg)' + 'translateX(-10px)' });
   // $(e.target).css('transform', 'translateX(-8px)');
@@ -23,4 +33,15 @@ $('#stand').on('mouseover', () => {
 });
 $('#sofa').on('mouseover', () => {
   $('#sofa_cotton').css({ display: 'block' });
+});
+
+let scroll = 0;
+$(window).on('scroll', (e) => {
+  let pos = $(e.target).scrollTop();
+  console.log(pos);
+  if (pos > 65) {
+    $('header').addClass('header_shadow');
+  } else {
+    $('header').removeClass('header_shadow');
+  }
 });
