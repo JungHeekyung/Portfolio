dfb14becf2d40e562c4379ff426b179972284089
JungHeekyung <kerryjhk@gmail.com>
2023-07-12 16:51:38 +0900

    about페이지 작업

--- about/script.js
+++ about/script.js
@@ -1,41 +1,4 @@
-$('#potion').on('click', () => {
-  $('#gray').fadeOut({ display: 'none' });
-});
 
-$('#clock').on('click', () => {
-  $('#desk_01').css({ opacity: '0' }),
-    $('#desk_02').css({ opacity: '0' }),
-    $('#laptop_error').css({ display: 'none' }),
-    $('#stand_light').css({ display: 'none' }),
-    $('#stand_on').css({ display: 'none' }),
-    $('#sofa_cotton').css({ display: 'none' }),
-    $('#art').css({ transform: 'rotate(0deg)' + 'translateX(0px)' }),
-    $('#gray').fadeIn({ display: 'block' });
-});
-$('#art').on('mouseover', (e) => {
-  $(e.target).css({ transform: 'rotate(5deg)' + 'translateX(-10px)' });
-  // $(e.target).css('transform', 'translateX(-8px)');
-});
-
-$('#desk_01').on('mouseover', (e) => {
-  $(e.target).css({ opacity: '1' });
-});
-$('#desk_02').on('mouseover', (e) => {
-  $(e.target).css({ opacity: '1' });
-  $('#desk_paper').css({ opacity: '1' });
-});
-$('#laptop').on('mouseover', () => {
-  $('#laptop_error').css({ display: 'block' });
-});
-$('#stand').on('mouseover', () => {
-  $('#stand_on').css({ display: 'block' });
-  $('#stand_light').css({ display: 'block' });
-});
-$('#sofa').on('mouseover', () => {
-  $('#sofa_cotton').css({ display: 'block' });
-});
-
-let scroll = 0;
 $(window).on('scroll', (e) => {
   let pos = $(e.target).scrollTop();
   if (pos > 65) {
@@ -43,4 +6,16 @@ $(window).on('scroll', (e) => {
   } else {
     $('header').removeClass('header_shadow');
   }
+});
+
+$('#potion').on('click', () => {
+  $('.title').addClass('active');
+  $('.illust').addClass('active');
+  $('.na_02').css({ display: 'block' });
+});
+
+$('#clock').on('click', () => {
+  $('.title').removeClass('active');
+  $('.illust').removeClass('active');
+  $('.na_02').css({ display: 'none' });
 });
