08a38de4be1e4c4c0673b102ec9d92d468913a21
JungHeekyung <kerryjhk@gmail.com>
2023-07-17 00:01:20 +0900

    설명페이지 사용기술 추가

---
+++ mrmen/script.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 5,
+    spaceBetween: 30,
+  
+    loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    }
+  });
+  
