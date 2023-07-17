802cd544362885e5d6e1a893459258bbd75d76ef
JungHeekyung <kerryjhk@gmail.com>
2023-07-17 11:56:01 +0900

    mrmen 수정

--- mrmen/script.js
+++ mrmen/script.js
@@ -1,11 +1,14 @@
 const swiper = new Swiper(".mySwiper", {
-    slidesPerView: 5,
-    spaceBetween: 30,
-  
-    loop: true,
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
-    }
-  });
-  
+  slidesPerView: 5,
+  spaceBetween: 0,
+
+/*   navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  }, */
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false
+  }
+});
