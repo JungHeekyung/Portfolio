86e80529ae72e36ff27d9b6b6065efd4eb9f6e68
JungHeekyung <kerryjhk@gmail.com>
2023-07-17 17:59:31 +0900

    mrmen 스와이퍼

--- mrmen/script.js
+++ mrmen/script.js
@@ -10,5 +10,22 @@ const swiper = new Swiper(".mySwiper", {
   autoplay: {
     delay: 2500,
     disableOnInteraction: false
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 3,
+      spaceBetween: 10
+    },
+    // when window width is >= 480px
+    768: {
+      slidesPerView: 5,
+      spaceBetween: 0
+    },
+    // when window width is >= 640px
+    1200: {
+      slidesPerView: 5,
+      spaceBetween: 10
+    }
   }
 });
