461ac6ed3e9aa3ce6dab8b56acb0ec64470c1370
JungHeekyung <kerryjhk@gmail.com>
2023-08-01 06:47:03 +0900

    바우하우스 스크립트

--- bauhaus/script.js
+++ bauhaus/script.js
@@ -4,8 +4,8 @@ const mainMovingImg = (evt) => {
   let x = evt.clientX;
   let y = evt.clientY;
 
-  $('.circle_outside').css({left:-50 - x /60, top:70 - y / 30});
-  $('.circle_inside').css({left:50% + x, top:50% + y});
+  $('.circle_outside').css({left:270+x/20, top:70+y/20,right:50+x/50,bottom:50+y/50});
+  $('.circle_inside').css({left:250+x/5, top:70+y/5,right:200+x/1,bottom:200+y/5});
 
   requestAnimationFrame(mainMovingImg);
 };
